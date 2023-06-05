e468218bda923b31325a6e60f5e613df4541c477
parwiz-hub <parwizyosefi786@gmail.com>
2023-06-05 05:58:42 -0700

    add Bloc Patern

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'bloc/cartListBloc.dart';
+
 
 void main() {
   runApp( MyApp());
@@ -9,13 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider(
+      blocs: [
+        Bloc((i)=>CartListBloc())
+      ],
+      child:  MaterialApp(
       title: 'Food Delivery',
       home: Scaffold(),
       debugShowCheckedModeBanner: false,
      
+    ),
     );
   }
 }
+
 
 
